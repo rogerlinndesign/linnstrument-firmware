c7c79294c67dd6ba20470ba746be1208a0372aea
ja-self <jas@jim-imac.attlocal.net>
2014-11-08 12:33:27 -0800

    Custom Messages
    Replaced generic scrolling messages with more personal - initial proof
    of concept

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -57,14 +57,14 @@ For any questions about this, contact Roger Linn Design at support@rogerlinndesi
 // FEEL FREE TO EDIT them, but note that not all characters might have a font representation.
 // Also, DO NOT CHANGE the number 16 nor remove or add any messages, you might crash the LinnStrument.
 char* audienceMessages[16] = {
-  "LINNSTRUMENT",
-  "APPLAUSE",
+  "THIS LINNSTRUMENT BELONGS TO JIM SELF",
+  "JAZELF@GMAIL.COM",
   "HA HA HA",
-  "SINGER SUCKS",
-  "WRONG NOTE",
+  "ASHES ASHES ALL FALL DOWN",
+  "RING AROUND THE ROSES",
   "SMELLY NIGHTCLUB",
   "HELLO",
-  "HELLO NEW YORK",
+  "HELLO DAVIS CALIFORNIA",
   "HELLO LOS ANGELES",
   "HELLO SAN FRANCISCO",
   "HELLO LONDON",
@@ -72,7 +72,8 @@ char* audienceMessages[16] = {
   "HELLO BRUSSELS",
   "HELLO PARIS",
   "HELLO TOKYO",
-  "HELLO (YOUR CITY HERE)"
+  "HELLO FINLEY AND RILO"
+  
 };
 
 char* OSVersion = "105.";
