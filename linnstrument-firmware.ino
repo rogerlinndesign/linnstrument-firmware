22b2e0e42ef07acd9a875085c0dda9cfaaf0c08d
Geert Bevin <gbevin@uwyn.com>
2014-12-31 01:40:05 +0100

    Using more samples for velocity detection, which we can now do since we're not waiting for a full surface scan per sample anymore. This stabilizes the values.

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -403,10 +403,10 @@ int32_t FXD4_DIV(int32_t a, int32_t b) {
 #define REAL_VELOCITY_CALCULATION 0
 
 #if REAL_VELOCITY_CALCULATION
-#define VELOCITY_SAMPLES 2
+  #define VELOCITY_SAMPLES 3
+  const int32_t VELOCITY_SCALE = FXD_FROM_INT(4);
 #else
-#define VELOCITY_SAMPLES 3
-const int32_t VELOCITY_SCALE = FXD_FROM_INT(4);
+  #define VELOCITY_SAMPLES 8
 #endif
 
 /***************************************** Calibration *******************************************/
