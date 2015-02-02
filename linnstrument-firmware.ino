1df0102c837c2d42c645f9a6543489543e5c4843
Geert Bevin <gbevin@uwyn.com>
2015-02-03 00:05:32 +0100

    Started 1.1.1 development

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -1,5 +1,5 @@
 /*=====================================================================================================================
-======================================== LinnStrument Operating System v1.1.0 =========================================
+======================================== LinnStrument Operating System v1.1.1 =========================================
 =======================================================================================================================
 
 Operating System for the LinnStrument (c) music controller by Roger Linn Design (www.rogerlinndesign.com).
@@ -77,7 +77,7 @@ char* audienceMessages[16] = {
   "HELLO (YOUR CITY HERE)"
 };
 
-char* OSVersion = "110";
+char* OSVersion = "111.";
 
 // SPI addresses
 const byte SPI_LEDS = 10;                // Arduino pin for LED control over SPI
