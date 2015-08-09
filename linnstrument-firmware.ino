10a516fb5019d10e941be07ba202d2ff532b4190
Geert Bevin <gbevin@uwyn.com>
2015-08-09 11:20:30 +0200

    Starting 1.2.3 development

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -54,8 +54,8 @@ For any questions about this, contact Roger Linn Design at support@rogerlinndesi
 
 /******************************************** CONSTANTS ******************************************/
 
-char* OSVersion = "122";
-char* OSVersionBuild = ".021";
+char* OSVersion = "123.";
+char* OSVersionBuild = ".022";
 
 // SPI addresses
 #define SPI_LEDS    10               // Arduino pin for LED control over SPI
