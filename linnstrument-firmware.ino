fe9c4408febf99681aa5c94eab84a7ec208272fd
Geert Bevin <gbevin@uwyn.com>
2015-09-27 21:04:26 +0200

    Updated build number to 24.

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -55,7 +55,7 @@ For any questions about this, contact Roger Linn Design at support@rogerlinndesi
 /******************************************** CONSTANTS ******************************************/
 
 char* OSVersion = "123.";
-char* OSVersionBuild = ".023";
+char* OSVersionBuild = ".024";
 
 // SPI addresses
 #define SPI_LEDS    10               // Arduino pin for LED control over SPI
