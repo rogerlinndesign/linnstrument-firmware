0daf1cc2fbc806107c88fa920079f978965d3aab
Geert Bevin <gbevin@uwyn.com>
2017-04-30 12:11:29 +0200

    Set version to 2.1.0 build 47

--- linnstrument-firmware.ino
+++ linnstrument-firmware.ino
@@ -56,8 +56,8 @@ For any questions about this, contact Roger Linn Design at support@rogerlinndesi
 
 /******************************************** CONSTANTS ******************************************/
 
-const char* OSVersion = "210.";
-const char* OSVersionBuild = ".046";
+const char* OSVersion = "210";
+const char* OSVersionBuild = ".047";
 
 // SPI addresses
 #define SPI_LEDS    10               // Arduino pin for LED control over SPI
